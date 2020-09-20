99c11de62f96421703e0dceb736b44b7dec16772
KruJeen <44271005+krujeen@users.noreply.github.com>
2020-09-20 10:53:46 +0700

    Create LED.py

---
+++ LED.py
@@ -0,0 +1,40 @@
+########## LED1.py ################
+from time import sleep
+from ipstw_ku import ipstw as iw
+while True:
+  led.value(0)
+  sleep(1)
+  led.value(1)
+  sleep(1)
+
+
+########## LED2.py ################
+from time import sleep
+from ipstw_ku import ipstw as iw
+while True:
+  led.value(0)
+  sleep(0.1)
+  led.value(1)
+  sleep(0.1)
+
+
+########## LED3.py ################
+from time import sleep
+from ipstw_ku import ipstw as iw
+while True:
+  led.value(0)
+  sleep(1)
+  led.value(1)
+  sleep(0.05)
+
+
+########## LED4.py ################
+from time import sleep
+from ipstw_ku import ipstw as iw
+from machine import Pin
+led = Pin(19,Pin.OUT)
+while True:
+  led.value(0)
+  sleep(1)
+  led.value(1)
+  sleep(1)
